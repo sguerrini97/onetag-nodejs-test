9516ea91691f76e793c84519bb3d85f222b75a07
onetag-dev <devops@onetag.com>
2023-03-14 12:19:42 +0100

    Fix books tests

--- __SEALED__/test/fixtures.js
+++ __SEALED__/test/fixtures.js
@@ -1,10 +1,12 @@
 const {MongoClient} = require('mongodb');
 const {fetch} = require('cross-fetch');
 const {until} = require('./utils');
+const {expect} = require('chai');
 
 exports.mochaGlobalSetup = async function () {
     const client = new MongoClient(`mongodb://localhost:${process.env.DATABASE_PORT}`)
     global['client'] = client;
+    global['expect'] = expect;
     await client.connect();
     global['database'] = client.db(process.env.DATABASE_NAME);
     await Promise.all([
--- __SEALED__/test/spec/books.js
+++ __SEALED__/test/spec/books.js
@@ -1,5 +1,4 @@
 const request = require('supertest');
-const {expect} = require('chai');
 
 describe('/books', () => {
 
@@ -8,25 +7,25 @@ describe('/books', () => {
     });
 
     it('POST responds with 200 on correct body', async () => {
-        const response = await request(`http://localhost:${process.env.SERVER_PORT}/`)
+        const response = await request(`http://localhost:${process.env.SERVER_PORT}`)
             .post('/books')
-            .send(JSON.stringify({
+            .set('Accept', 'application/json')
+            .send({
                 "name": "Treasure Island",
                 "author": "Robert Louis Stevensonr",
                 "publisher": "Cassell and Company",
                 "edition": 1,
                 "pages": 292,
                 "releaseDate": "1983-11-14"
-            }))
-            .expect('Content-Type', 'application/json')
+            })
+            .expect('Content-Type', /json/)
             .expect(200);
-        const document = JSON.parse(response.body);
-        expect(document.id).to.exist;
-        expect(document.id).to.be.a('string');
+        expect(response.body.id).to.exist;
+        expect(response.body.id).to.be.a('string');
     });
 
     it('POST responds with 400 on invalid body', async () => {
-        const response = await request(`http://localhost:${process.env.SERVER_PORT}/`)
+        const response = await request(`http://localhost:${process.env.SERVER_PORT}`)
             .post('/books')
             .send(JSON.stringify({
                 "publisher": "Cassell and Company",
@@ -47,7 +46,7 @@ describe('/books', () => {
                 "pages": 292,
                 "releaseDate": "1983-11-14"
             }))
-            .expect('Content-Type', 'application/json')
+            .expect('Content-Type', /json/)
             .expect(200);
         const document = await database.collection('books').findOne({ _id: response.body.id });
         expect(document).not.to.be.null;
