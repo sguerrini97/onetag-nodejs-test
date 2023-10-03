bb79883cb5cbaab373e48338c12d0566f2af0bb2
onetag-dev <devops@onetag.com>
2023-10-03 10:27:22 +0200

    Remove not found services warning

--- __SEALED__/test/fixtures.js
+++ __SEALED__/test/fixtures.js
@@ -1,25 +1,18 @@
 const {MongoClient} = require('mongodb');
 const {fetch} = require('cross-fetch');
-const {until, timeout} = require('./utils');
+const {until} = require('./utils');
 const {expect} = require('chai');
 
 exports.mochaGlobalSetup = async function () {
     const client = new MongoClient(`mongodb://database:${process.env.DATABASE_PORT}`)
-    const result = await Promise.race([
-        timeout(3000).then(() => 'timeout'),
+    await Promise.all([
+        client.connect(),
         Promise.all([
-            client.connect(),
-            Promise.all([
-                process.env.AUTH_PORT,
-                process.env.SONGS_PORT,
-                process.env.SERVER_PORT,
-            ].map((port) => until(() => fetch(`http://server:${port}`))))
-        ]),
+            process.env.AUTH_PORT,
+            process.env.SONGS_PORT,
+            process.env.SERVER_PORT,
+        ].map((port) => until(() => fetch(`http://server:${port}`))))
     ]);
-    if (result === 'timeout') {
-        await client.close(true);
-        throw new Error('Database unreachable, have you called npm start?');
-    }
     global['client'] = client;
     global['expect'] = expect;
     global['database'] = client.db(process.env.DATABASE_NAME);
