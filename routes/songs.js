286f03a712a185a8bdbeea54707269ec948dc9b7
Samuele Guerrini <sguerrini97@gmail.com>
2023-10-15 12:15:40 +0200

    Replace axios with cross-fetch to avoid extra dependency

--- routes/songs.js
+++ routes/songs.js
@@ -1,4 +1,4 @@
-const axios = require('axios');
+const fetch = require('cross-fetch');
 const express = require('express');
 const router = express.Router();
 
@@ -20,6 +20,20 @@ class AccessToken {
 }
 const _accessTokens = new Map();
 
+const fetchJson = async function(url, headers = {}, params = {}) {
+
+    // Add query parameters to the URL
+    url = `${url}?${new URLSearchParams(params)}`;
+
+    const response = await fetch(url, {
+        headers: headers,
+    });
+
+    const json = await response.json();
+
+    return json;
+}
+
 /**
  * Retrieve an access token from the Auth service or returns the last one if it's not expired.
  * @param {String} version Version of the access token to retrieve. Defaults to V1.
@@ -27,12 +41,9 @@ const _accessTokens = new Map();
  */
 const getAccessToken = async function(version = "V1") {
     if (!_accessTokens.has(version) || _accessTokens.get(version).isExpired()) {
-        const accessTokenResponse = (await axios({
-            method: 'GET',
-            url: AUTH_SERVICE_URL,
-        }))?.data;
-
-        _accessTokens.set(version, new AccessToken(accessTokenResponse[`TOKEN-${version}`]));
+        const accessTokenResponse = await fetchJson(AUTH_SERVICE_URL);
+        const token = accessTokenResponse[`TOKEN-${version}`];
+        _accessTokens.set(version, new AccessToken(token));
     }
 
     return _accessTokens.get(version);
@@ -45,13 +56,9 @@ router.get('/', async (req, res) => {
         const accessToken = await getAccessToken('V1');
 
         // Retrieve the total number of songs
-        let songsCount = (await axios({
-            method: 'GET',
-            url: `${SONGS_SERVICE_URL}/count`,
-            headers: {
-                'TOKEN-V1': accessToken.token,
-            },
-        }))?.data?.count ?? 0;
+        let songsCount = (await fetchJson(`${SONGS_SERVICE_URL}/count`, {
+            'TOKEN-V1': accessToken.token,
+        }))?.count ?? 0;
 
         const genres = new Map();
 
@@ -59,17 +66,12 @@ router.get('/', async (req, res) => {
         while (songsCount > 0) {
 
             // Retrieve a page of songs
-            const songsPage = (await axios({
-                method: 'GET',
-                url: `${SONGS_SERVICE_URL}/`,
-                params: {
-                    limit: SONGS_PER_PAGE,
-                    offset: (page++) * SONGS_PER_PAGE,
-                },
-                headers: {
-                    'TOKEN-V1': accessToken.token,
-                },
-            }))?.data;
+            const songsPage = await fetchJson(`${SONGS_SERVICE_URL}/`, {
+                'TOKEN-V1': accessToken.token,
+            }, {
+                limit: SONGS_PER_PAGE,
+                offset: (page++) * SONGS_PER_PAGE,
+            });
 
             for (const song of songsPage) {
                 // Make sure this song's genre is in the genres Map
